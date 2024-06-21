064e1dc225fb708deed4933e4976e904b3eba403
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-21 14:47:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+class ‘Movie’
+{
+    public $nome;
+    public $regista;
+    public $durata;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+
+<body>
+<h1>ciao</h1>
+</body>
+
+</html>
